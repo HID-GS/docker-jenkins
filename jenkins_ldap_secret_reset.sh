2bac3cc445737c2601796f529c31a5fc88580802
Peter Carrero <peter.carrero@gmail.com>
2018-11-05 13:25:00 -0600

    Add ldap password reset script

---
+++ jenkins_ldap_secret_reset.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ ${JENKINS_HOME+x} ] && [ ${LDAP_SECRET+x} ] && [ -f "${JENKINS_HOME}/config.xml" ]; then
+  echo "found config.xml file, adjusting LDAP password"
+  sed -i.bak "s#\(<managerPasswordSecret>\).*\(</managerPasswordSecret>\)#\1${LDAP_SECRET}\2#g" "${JENKINS_HOME}/config.xml"
+else
+  echo "config.xml not found"
+fi

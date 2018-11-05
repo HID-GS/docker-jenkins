ae47f9129fe77c1f933ada8f9be99f3104c70df0
Peter Carrero <peter.carrero@gmail.com>
2018-11-05 15:44:45 -0600

    Add support for docker secrets

--- jenkins_ldap_secret_reset.sh
+++ jenkins_ldap_secret_reset.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-if [ ${JENKINS_HOME+x} ] && [ ${LDAP_SECRET+x} ] && [ -f "${JENKINS_HOME}/config.xml" ]; then
+if [ ${LDAP_SECRET+x} ]; then
+  JENKINS_LDAP_SECRET=${LDAP_SECRET}
+fi
+
+if [ -f "/run/secrets/jenkins_ldap_password" ]; then
+  JENKINS_LDAP_SECRET=$(cat /run/secrets/jenkins_ldap_password)
+fi
+
+if [ ${JENKINS_HOME+x} ] && [ ${JENKINS_LDAP_SECRET+x} ] && [ -f "${JENKINS_HOME}/config.xml" ]; then
   echo "found config.xml file, adjusting LDAP password"
-  sed -i.bak "s#\(<managerPasswordSecret>\).*\(</managerPasswordSecret>\)#\1${LDAP_SECRET}\2#g" "${JENKINS_HOME}/config.xml"
+  sed -i.bak "s#\(<managerPasswordSecret>\).*\(</managerPasswordSecret>\)#\1${JENKINS_LDAP_SECRET}\2#g" "${JENKINS_HOME}/config.xml"
 else
   echo "config.xml or ldap settings not found"
 fi

4c996b3afe3d7034fdf3b9ea07dbf6b96eec21cf
Peter Carrero <peter.carrero@gmail.com>
2018-11-05 13:26:32 -0600

    Adjust log message for missing parameters

--- jenkins_ldap_secret_reset.sh
+++ jenkins_ldap_secret_reset.sh
@@ -4,5 +4,5 @@ if [ ${JENKINS_HOME+x} ] && [ ${LDAP_SECRET+x} ] && [ -f "${JENKINS_HOME}/config
   echo "found config.xml file, adjusting LDAP password"
   sed -i.bak "s#\(<managerPasswordSecret>\).*\(</managerPasswordSecret>\)#\1${LDAP_SECRET}\2#g" "${JENKINS_HOME}/config.xml"
 else
-  echo "config.xml not found"
+  echo "config.xml or ldap settings not found"
 fi
